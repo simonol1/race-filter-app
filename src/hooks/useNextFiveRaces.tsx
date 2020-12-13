7d1f62898356c17b9ccdc486e2844f0fb0bd4ca8
Simon O'Leary <simondanolearyis@gmail.com>
2020-12-14 06:38:58 +1000

    fix, truncating the race list before sort when no filter

--- src/hooks/useNextFiveRaces.tsx
+++ src/hooks/useNextFiveRaces.tsx
@@ -12,7 +12,17 @@ const horse =	'4a2788f8-e825-4d36-9894-efd4baf1cfae';
 const harness = '161d9be2-e909-4326-8c2c-35ed71fb460b';
 
 
-/*
+/* Sort the races by time and truncate to 5 races
+ * @param {array} races: a set of objects storing the data for the next 25 races 
+ * @returns Array of filtered and sorted objects 
+ */
+
+const getSortedNextFiveRaces = (races: any) => {
+	const sortedRaces = races.sort((a: any, b: any) => a?.advertised_start?.seconds - b?.advertised_start?.seconds);
+	return sortedRaces.slice(0,5);
+}
+
+/* Handle race filtering
  * @param {array} nextFiveRaces: a set of objects storing the data for the next 25 races 
  * @param {object} filterProps: Object of filter states
  * @returns Array of filtered and sorted objects 
@@ -24,10 +34,9 @@ const getFilteredRaceData = (nextFiveRaces: any, {...filterProps}) => {
 	const showHorse = filterProps?.showHorse;
 	const showGreyhound = filterProps?.showGreyhound;
 	const filteredNextFiveRaces = nextFiveRaces.filter((r:any) => r.advertised_start?.seconds + 60 > (Date.now() / 1000));
-	const nextFiveAllRaces = filteredNextFiveRaces.slice(0,5);
 	
 	if (!showHarness && !showHorse && !showGreyhound) {
-		return nextFiveAllRaces.sort((a: any, b: any) => a?.advertised_start?.seconds - b?.advertised_start?.seconds)
+		return getSortedNextFiveRaces(filteredNextFiveRaces);
 	} 
 
 	if (showGreyhound) {
@@ -52,10 +61,10 @@ const getFilteredRaceData = (nextFiveRaces: any, {...filterProps}) => {
 		});
 	}
 
-	return filteredRaces.sort((a: any, b: any) => a?.advertised_start?.seconds - b?.advertised_start?.seconds).slice(0,5); 
+	return getSortedNextFiveRaces(filteredRaces); 
 };
 
-/*
+/* Get the race data and set it to state
  * @param {object} filterProps: Object of filter states
  * @returns Array of filtered and sorted objects 
  */
